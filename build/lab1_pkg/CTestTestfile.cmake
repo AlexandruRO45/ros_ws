084e9f2c24b2480247a62fefde8c7ef9ae94f61b
Alex <savaalexandru562@gmail.com>
2024-03-13 15:30:12 +0000

    Delete build directory

--- build/lab1_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vagrant/ros_ws/src/lab1_pkg
-# Build directory: /home/vagrant/ros_ws/build/lab1_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
