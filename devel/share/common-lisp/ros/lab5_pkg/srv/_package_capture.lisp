f8b1665c768303a2ef24545409f80196cb68c38d
Alex <savaalexandru562@gmail.com>
2024-03-13 15:30:26 +0000

    Delete devel directory

--- devel/share/common-lisp/ros/lab5_pkg/srv/_package_capture.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package lab5_pkg-srv)
-(cl:export '(CAPTURE-VAL
-          CAPTURE
-          SUCCESS-VAL
-          SUCCESS
-))
--- devel/share/gennodejs/ros/lab5_pkg/srv/_index.js
+++
@@ -1,8 +0,0 @@
-
-"use strict";
-
-let capture = require('./capture.js')
-
-module.exports = {
-  capture: capture,
-};
